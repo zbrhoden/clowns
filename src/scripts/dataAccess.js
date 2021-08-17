91ab83c447c11cda3f216a4e6a6ae90acb4f9077
Blake Rhoden <zbrhoden@gmail.com>
2021-08-17 15:52:42 -0500

    applicationstate

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,1 +1,11 @@
+const applicationState = {
+    reservations: []
+}
 const API = "http://localhost:8088"
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
